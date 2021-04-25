b1a479ee18f8a4c6c96758832e8155eea7c177bf
Aymane Lassfar <lassfar@users.noreply.github.com>
2021-04-25 23:26:59 +0000

    removed_@ccalamos/gatsby-googlemaps

---
+++ src/pages/news.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import SectionTitle from 'components/layouts/section-title'
+import Layout from 'components/layouts/layout'
+
+const News = () => {
+  return (
+    <Layout>
+      <SectionTitle title="Actualités" bg="bg_black" />
+    </Layout>
+  )
+}
+
+export default News
